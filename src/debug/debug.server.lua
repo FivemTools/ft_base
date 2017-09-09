84e283e97c3b72aedb4bfcf80d18e3cd2f231366
Samuel Ds <samuelds.pro@gmail.com>
2017-09-09 21:45:29 +0200

    Add debug event

---
+++ src/debug/debug.server.lua
@@ -0,0 +1,13 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+-- @Source: https://github.com/FivemTools/ft_base
+--
+
+RegisterServerEvent("ft_base:debug")
+AddEventHandler('ft_base:debug', function(value)
+
+  tprint(value)
+
+end)
