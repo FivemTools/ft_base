5a97143c8ab5cc6bf5deff902859347ed36c09e6
Samuel Ds <samuelds.pro@gmail.com>
2017-09-09 14:50:44 +0200

    Add resource file

---
+++ __resource.lua
@@ -0,0 +1,22 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+-- @Source: https://github.com/FivemTools/ft_base
+--
+
+dependencies {
+  "mysql-async",
+}
+
+client_scripts {}
+
+server_scripts {
+
+    "@mysql-async/lib/MySQL.lua", -- MySQL
+
+}
+
+exports {}
+
+server_exports {}
