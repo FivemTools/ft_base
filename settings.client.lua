76c2927030ab2e84e809efdd40673b9b59bacdeb
Samuel Ds <samuelds.pro@gmail.com>
2017-09-11 19:54:00 +0200

    Add client settings

---
+++ settings.client.lua
@@ -0,0 +1,18 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+-- @Source: https://github.com/FivemTools/ft_base
+--
+
+Settings = {
+  defaultSpawn = {
+    posX = 0,
+    posY = 0,
+    posZ = 0,
+    heading = 0,
+    model = "a_m_y_skater_01",
+  },
+  sendPos = 1, -- minutes
+  savePos = 10, -- minutes
+}
